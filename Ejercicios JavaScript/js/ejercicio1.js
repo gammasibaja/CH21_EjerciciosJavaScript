1baea662f5ff893d2ce2462106d3127fa07ddede
Gamaliel Sibaja Ibarra <116774389+gammasibaja@users.noreply.github.com>
2022-12-11 19:37:56 -0600

    Add files via upload
    Se crearon los archivos indexi.html, y están ligados a los ejercicios 1 y 2 que ya son funcionales

---
+++ Ejercicios JavaScript/js/ejercicio1.js
@@ -0,0 +1,10 @@
+//Escribir una función que tome un arreglo de números y devuelva el número mayor de la lista. Ejemplo del arreglo : [3, 5, 7, 1, 6]
+
+let arreglo = [3, 5, 7, 1, 6]
+
+function max_arreglo (){
+    console.log(Math.max(...arreglo));
+
+}
+
+max_arreglo ();
---
+++ Ejercicios JavaScript/js/ejercicio3.js
@@ -0,0 +1,16 @@
+//Escribir una función que permita saber si un número se repite dentro de un arreglo.
+// Caso 1: Resultado esperado: El número 5 se encuentra más de una vez [1, 22, 5, 17, 10, 5, 40, 5]
+// Caso 2: Resultado esperado: El número 7 se encuentra más de una vez [7, 41, 5, 7, 10, 13, 2]
+// Caso 3: Resultado esperado: No hay números repetidos [1, 22, 5, 14, 24, 31, 27, 15, 105]
+
+let caso1 = [1, 22, 5, 17, 10, 5, 40, 5]; 
+let caso2 = [7, 41, 5, 7, 10, 13, 2];
+let caso3 = [1, 22, 5, 14, 24, 31, 27, 15, 105];
+
+// function repetidos () {
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+        
+//     }
+    
+// }
